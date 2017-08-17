473159bf7f87861d94a7aaecbb725df27e5cbce2
dongwoLee <dongwoo4553@gmail.com>
2017-08-17 21:28:15 +0900

    example4

--- missingValue.py
+++ missingValue.py
@@ -1,3 +1,33 @@
 import csv
 import pandas as pd
+import datetime
+
+startDate=datetime.date(2012,5,7)
+endDate = datetime.date(2012,10,31)
+
+dateList = []
+dateRange=pd.date_range(startDate,endDate)
+
+for single_date in dateRange:
+    dateList.append((single_date.strftime("%Y.%m.%d")))
+
+data = pd.read_csv('Yang_WaterPump.csv')
+list = (data.values.tolist())
+
+a=[]
+
+for i in range(len(list)):
+    a.append(list[i][0])
+
+b =set(dateList)-set(a)
+print (b)
+
+
+
+
+
+
+
+
+
 
