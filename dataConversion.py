864746f65012ee322bc126f93608d8ecb88b0a5d
dongwoLee <dongwoo4553@gmail.com>
2017-08-22 00:22:28 +0900

    add mosquito level

--- dataConversion.py
+++ dataConversion.py
@@ -1,8 +1,19 @@
 import pandas as pd
 import datetime,time
 
-standardDate = datetime.date(2012,6,1)
-endDate = datetime.date(2012,7,1)
+# standardDate = datetime.date(2012,6,1)
+# endDate = datetime.date(2012,7,1)
+#
+# dateRange = pd.date_range(standardDate,endDate)
+#
+# with open('integration.csv','r') as myfile:
+#
+# for single_date in dateRange:
+#     tempDate = []
+#     for i in range(0,31):
+#         rangeDate = single_date+datetime.timedelta(days=-i)
+#         tempDate.append(rangeDate.strftime("%Y.%-m.%-d"))
+
 
 
 
